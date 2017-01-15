1725178eae3882491a22a5f8682c1759ba87de27
kg6ka <kg6ka5@gmail.com>
2017-01-15 17:28:45 +0200

    node_dev added start page loader

--- app.js
+++ app.js
@@ -3,12 +3,33 @@
 var server = require('./server');
 var config = require('./config');
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-http.createServer(isServer).listen(config.server.port);
+var mimeTypes = {
+    "html": "text/html"
+};
 
-function isServer(req, res) {
-    res.writeHead(200);
-    res.end(server('NodeJS'));
+http.createServer(handleRequest).listen(config.server.port, console.log('Node started on http://%s:%s', config.server.host, config.server.port));
+
+function handleRequest(req, res) {
+    var uri = url.parse(req.url).pathname;
+
+    if(uri === '/') uri += 'index.html';
+
+    var filename = path.join(__dirname, '/frontend/', uri);
+
+    fs.access(filename, fs.F_OK, function(err) {
+        if(err) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('404 Not Found\n');
+            return res.end();
+        }
+
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        res.writeHead(200, {'Content-Type':mimeType});
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+    });
 }
-
-console.log('Node started on http://' + config.server.host + ':' + config.server.port);
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     server: {
         host: '127.0.0.1',
-        port: '7000',
+        port: '7001',
         baseURL: "http://127.0.0.1:7000"
     }
 };
