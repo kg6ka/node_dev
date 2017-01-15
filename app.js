8ccddb13d2ed4033d8861190cbf672dea5c42b7c
kg6ka <kg6ka5@gmail.com>
2017-01-15 17:34:19 +0200

    node_dev updated app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var server = require('./server');
 var config = require('./config');
 var http = require('http');
 
-http.createServer(isServer).listen(config.server.port);
+http.createServer(handleRequest)
+    .listen(config.server.port,
+        console.log('Node started on http://%s:%s', config.server.host, config.server.port));
 
-function isServer(req, res) {
+function handleRequest(req, res) {
     res.writeHead(200);
     res.end(server('NodeJS'));
 }
-
-console.log('Node started on http://' + config.server.host + ':' + config.server.port);
